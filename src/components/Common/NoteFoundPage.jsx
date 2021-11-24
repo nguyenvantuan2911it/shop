3be3e436294951772587bb74dcbfee447361bf5f
Van Tuan <nguyenvantuan972.it@gmail.com>
2021-11-24 15:43:52 +0700

    first commit

---
+++ src/components/Common/NoteFoundPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+NoteFoundPage.propTypes = {};
+
+function NoteFoundPage(props) {
+  return <div>404! Khom tim thay trang</div>;
+}
+
+export default NoteFoundPage;
---
+++ src/features/Search/index.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import SearchItem from "./components/SearchItem";
+import { searchActions } from "./SearchSlice";
+Search.propTypes = {};
+
+function Search(props) {
+  const dispatch = useDispatch();
+  const { name } = useParams();
+  const [name_like, keyword] = name.split("=");
+  const SearchList = useSelector((state) => state.search.list);
+  useEffect(() => {
+    dispatch(searchActions.fetchData({ [name_like]: keyword }));
+  }, [keyword, dispatch, name_like]);
+  return <div>{SearchList && <SearchItem SearchList={SearchList} />}</div>;
+}
+
+export default Search;
